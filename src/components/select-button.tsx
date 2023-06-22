24151b96219f63fcfadd2eed08e693490a8e00e4
cuongcode <congcuongnguyen.dev@gmail.com>
2023-06-23 00:52:35 +0700

    add condition to some components

--- src/components/select-button.tsx
+++ src/components/select-button.tsx
@@ -1,10 +1,10 @@
 
-export const SelectButton = () => {
+export const SelectButton = ({isSelected}:{isSelected:any}) => {
     return (
       <>
         <button>
           <div className='flex border-2 rounded-md border-[#309a87] w-4 h-4 justify-center items-center'>
-            <div className='bg-[#309a87] rounded-md w-2 h-2'></div>
+            {isSelected ? <div className='bg-[#309a87] rounded-md w-2 h-2'></div> : null}
           </div>
         </button>
       </>
--- src/components/task-card.tsx
+++ src/components/task-card.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   SelectButton,
   OutlineStarButton,
@@ -10,15 +12,18 @@ import {
 } from "./index";
 
 export const TaskCard = ({ task }: { task: any }) => {
+  const [isPinned, setIsPinned] = useState(false);
+  const [isShowSelectBox, setIsShowSelectBox] = useState(true);
+  const [isSelected, setIsSelected] = useState(false)
+
   return (
     <>
       <div className="flex flex-col py-2 px-3 bg-white space-y-4 rounded-lg">
         <div className="flex justify-between items-center text-base">
           <div className="flex items-center space-x-1">
-            <SelectButton />
-            <OutlineStarButton />
-            <SolidStarButton />
-            <div>{task.title}</div>
+            {isShowSelectBox ? <SelectButton isSelected={isSelected}/> : null}
+            {isPinned ? <SolidStarButton /> : <OutlineStarButton />}
+            <div className="">{task.title}</div>
             <PencilEditButton />
           </div>
           <div className="flex flex-col">
@@ -29,7 +34,11 @@ export const TaskCard = ({ task }: { task: any }) => {
         <div className="text-xs text-gray-400">{task.description}</div>
         <div className="flex space-x-1">
           <TagButton />
-          {task.tags.map((item:any) => <div key={item.name}><Tag tag={item}/></div>)}
+          {task.tags.map((item: any) => (
+            <div key={item.name}>
+              <Tag tag={item} />
+            </div>
+          ))}
         </div>
       </div>
     </>
