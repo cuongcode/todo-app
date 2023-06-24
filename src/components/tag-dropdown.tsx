3885e48d47546c398d02ae41c19fb380e85e3ed0
cuongcode <congcuongnguyen.dev@gmail.com>
2023-06-24 10:52:23 +0700

    split new tag form component

--- src/components/tag-dropdown.tsx
+++ src/components/tag-dropdown.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-import { TagIcon,PlusIcon } from "@heroicons/react/24/outline";
+import { TagIcon } from "@heroicons/react/24/outline";
 
-import { Tag } from "./index";
+import { Tag, NewTagForm } from "./index";
 
 export const TagDropdown = () => {
   const [isDropdown, setIsDropdown] = useState(false);
@@ -20,21 +20,15 @@ export const TagDropdown = () => {
       </button>
       {isDropdown ? (
         <div className="z-10 absolute top-5 flex flex-col space-y-2 bg-[#e9f2f1] p-2 rounded-lg w-fit">
-          <div className="flex space-x-2">
-            <input className="w-4/6 rounded-lg" type="text"></input>
-            <div className="flex w-2/6">
-              <button className="w-1/2 bg-red-300 rounded-lg"></button>
-              <button type="button" className="w-1/2"><PlusIcon className="m-auto w-4 text-[#309a87]"/></button>
-            </div>
-          </div>
+          <NewTagForm />
           <div className="flex flex-wrap space-x-1 space-y-1 items-baseline">
             <Tag tag={{ name: "read", color: "red" }} />
             <Tag tag={{ name: "play", color: "blue" }} />
             <Tag tag={{ name: "chill", color: "green" }} />
-            <Tag tag={{ name: "watch", color: "yellow" }} />
-            <Tag tag={{ name: "watch", color: "yellow" }} />
-            <Tag tag={{ name: "watch", color: "yellow" }} />
-            <Tag tag={{ name: "watch", color: "yellow" }} />
+            <Tag tag={{ name: "watch", color: "orange" }} />
+            <Tag tag={{ name: "watch", color: "amber" }} />
+            <Tag tag={{ name: "watch", color: "teal" }} />
+            <Tag tag={{ name: "watch", color: "cyan" }} />
           </div>
         </div>
       ) : null}
